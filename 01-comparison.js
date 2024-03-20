9b15b38cf372108a19ab3eb4552d8844b0d9a39d
54ntu <chaudharysantaram8@gmail.com>
2024-03-20 16:08:52 +0545

    heap && stack detail

--- 01-comparison.js
+++ 01-comparison.js
@@ -5,3 +5,16 @@ console.log("2"===2);
 console.log("2"==2);
 
 // "==" only compare the value not datatype
+
+
+//what is the result of null datatypes if we check using type0f function : object
+
+let num1 = null;
+console.log(typeof(num1));
+
+
+// Array in js == list in python 
+// objects in js == dictionary in python 
+
+
+
---
+++ memory.js
@@ -0,0 +1,8 @@
+// two types of memory
+
+// stack and heap memory
+
+// stack memory is used in primitive datatypes
+// heap in non-primitive
+
+
