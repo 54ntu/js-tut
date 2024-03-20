49eb556a4fea3e15bd468c26b2453c0e8c7144b6
54ntu <chaudharysantaram8@gmail.com>
2024-03-20 15:48:46 +0545

    comparison between === && ==

---
+++ 01-comparison.js
@@ -0,0 +1,7 @@
+console.log("2"===2);
+//" ===" is used for strict comparison which compare not only the value but also its datatypes4
+
+
+console.log("2"==2);
+
+// "==" only compare the value not datatype
