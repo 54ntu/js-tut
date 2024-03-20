15caf70b1bd6a8818506beb614b9c7adfb9d070a
54ntu <chaudharysantaram8@gmail.com>
2024-03-20 18:07:26 +0545

    objects in array

---
+++ objects.js
@@ -0,0 +1,16 @@
+//singleton -> if we create objects using constructor then it is singleton
+
+
+//object literals
+
+const JsUser ={
+    name:"lol",
+    age:20,
+    location:'ktm',
+    email :'lol@google.com',
+    isLoggedIn:false,
+}
+
+console.log(JsUser['name']);
+console.log(JsUser["age"]);
+console.log(JsUser["location"]);
