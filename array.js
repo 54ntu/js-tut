a0b84524eba4da8791f175a514871dce0105e155
54ntu <chaudharysantaram8@gmail.com>
2024-03-20 17:26:22 +0545

    array in javascript

---
+++ array.js
@@ -0,0 +1,14 @@
+//array
+//change in array has effects on two ways "shallow copy" and "deep copy"
+
+const myArr= ['football','cricket','basketball',10,20]
+console.log(myArr[3]);
+
+// array methods in javascript
+
+myArr.push('lol');
+console.log(myArr);
+
+myArr.unshift(1) 
+console.log(myArr);
+
