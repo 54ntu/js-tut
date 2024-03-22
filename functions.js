f6f3fa060bc236bb65394b0b2e44a459027c4e4b
54ntu <chaudharysantaram8@gmail.com>
2024-03-22 08:25:18 +0545

    functions in js

---
+++ functions.js
@@ -0,0 +1,15 @@
+// function addTwoNumbers(num1, num2){
+//     console.log(num1+num2);
+// }
+
+// addTwoNumbers(2,4);
+
+
+
+///rest operator or spread operator
+
+function calculatePrice(...price){
+    return price
+}
+
+console.log(calculatePrice(200,300));
