c1574913388c24572a99cb51e1fdef2208f47bb7
54ntu <chaudharysantaram8@gmail.com>
2024-03-22 14:06:21 +0545

    oeprator && controlflow

---
+++ controlflow.js
@@ -0,0 +1,61 @@
+//if statement
+
+// if(condition){
+//     console.log('statement that executes here')
+// }
+
+
+// const balance = 1000;
+// if(balance>500) console.log('test1'),
+// console.log('test2'); //this is not a good way to write a code
+
+
+
+///************switch case statement******* */
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log('january') 
+        break;
+    case 2:
+        console.log('february') 
+        break;
+    case 3:
+        console.log('march') 
+        break;
+    case 4:
+        console.log('April') 
+        break;
+
+    default:
+        console.log('nothing there to match')
+        break;
+}
+
+
+//**********nullish coalescing operator(??): null undefined***** */
+
+let val1;
+val1 = 5 ?? 10;
+
+console.log(val1);
+
+
+
+////***ternary operator***** */
+
+// condition ?true : false; 
+
+
