cf8084e939071ce3be36f89770f8574d2ead5d66
54ntu <chaudharysantaram8@gmail.com>
2024-03-22 10:40:00 +0545

    function file is updated

--- functions.js
+++ functions.js
@@ -8,8 +8,22 @@
 
 ///rest operator or spread operator
 
-function calculatePrice(...price){
-    return price
-}
+// function calculatePrice(...price){
+//     return price
+// }
 
-console.log(calculatePrice(200,300));
+// console.log(calculatePrice(200,300));
+
+
+
+// function compareAge(age1, age2){
+//     if(age1>age2){
+//         return `${age1} is greater than ${age2}`
+//     }
+//     else{
+//         return `age2 ${age2} is greater than age1 ${age1}`
+//     }
+// }
+
+
+// console.log(compareAge(10,20));
