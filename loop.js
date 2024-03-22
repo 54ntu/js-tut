3656c9f409433e76ad11f20c2d7bb097e5c2b1ad
54ntu <chaudharysantaram8@gmail.com>
2024-03-22 14:32:22 +0545

    for loop in js

---
+++ loop.js
@@ -0,0 +1,14 @@
+//for loop
+
+for (let index = 0; index < 10; index++) {
+    const element =index;
+    console.log(index);
+    
+}
+
+
+
+///break and continue
+
+//when break is encoutered then execution of whole program is haulted
+//when continue is encountered then execution of loop is haulted by once then again execution starts
