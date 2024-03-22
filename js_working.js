f615087e82ee7f3d51574099aec832aeb2bc6691
54ntu <chaudharysantaram8@gmail.com>
2024-03-22 13:17:28 +0545

    js working

---
+++ js_working.js
@@ -0,0 +1,11 @@
+//how does code execute in javascript
+
+// javascript execution context 
+// ->>>> Global execution context 
+// --->>>function execution context 
+// -->> Eval execution context
+
+//////////memory creation phase
+////////execution phase
+///////
+
