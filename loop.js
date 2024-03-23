bf721bcc1a63794c8db3ff2f52cdcec4818c299e
54ntu <chaudharysantaram8@gmail.com>
2024-03-23 12:36:05 +0545

    dom manipulation

--- loop.js
+++ loop.js
@@ -1,10 +1,10 @@
 //for loop
 
-for (let index = 0; index < 10; index++) {
-    const element =index;
-    console.log(index);
+// for (let index = 0; index < 10; index++) {
+//     const element =index;
+//     console.log(index);
     
-}
+// }
 
 
 
@@ -12,3 +12,42 @@ for (let index = 0; index < 10; index++) {
 
 //when break is encoutered then execution of whole program is haulted
 //when continue is encountered then execution of loop is haulted by once then again execution starts
+
+
+///************while loop and do-while loop**** */
+
+// while (condition) {
+
+//     statement
+// }
+
+
+
+//*** forof loop ****/
+
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+
+const greetings = 'hello world!'
+for (const greet of greetings) {
+    // console.log(greet);
+    
+}
+
+
+//forin loop
+
+const myobject={
+    js:'javascript',
+    cpp:'c++',
+    py:'python',
+}
+
+for (const key in myobject) {
+  console.log(key)
+}
+
