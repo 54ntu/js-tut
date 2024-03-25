4ffcad6658cc0641ce706319702d59533a3d23f7
54ntu <chaudharysantaram8@gmail.com>
2024-03-25 14:48:47 +0545

    setTimeout && clearTimeout in js

---
+++ asyncCode.js
@@ -0,0 +1,15 @@
+///javascript is synchronous and single threaded
+///->>>> synchronous means code execution by one by one
+//// ->>> single threaded means code execution by sinle thread
+
+
+////-->>>execution context
+    ///--> execute one line of code at a time
+    ///-->>>console log->1
+    ///--->>>console log->2  each operation waits for the last one to complete before excuting
+
+
+//////// Blocking code vs non blocking code
+
+///blocking code --->>block the flow of program --> read file sync
+///Non blocking code ---->>> donot block excution --->>>>read file async
