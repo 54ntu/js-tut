28f606b89aa5e30f36d558cdea55ed6ae62aafba
54ntu <chaudharysantaram8@gmail.com>
2024-03-26 11:06:26 +0545

    fetch api in js

---
+++ fetch.js
@@ -0,0 +1,12 @@
+fetch("https://api.github.com/users/54ntu")
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+
